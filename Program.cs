a35ac42679b859fca1bc0a6b21f0ad92873a96e8
Eddy Ahlqvist <eddy.ahlqvist@protonmail.com>
2026-07-23 18:44:21 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pigeonochi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Pigeon!");
+        }
+    }
+}
